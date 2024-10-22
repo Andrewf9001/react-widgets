b30105c53f38768e4dc9be1d7d025fc3881bb61d
Andrewf9001 <andrewf9001@gmail.com>
2024-10-22 15:22:25 -0600

    added homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+
 import BoxShadowPage from "./pages/BoxShadowPage";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
     <div className="app-container">
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/box-shadow" element={<BoxShadowPage />} />
       </Routes>
     </div>
