30195731aee183e73a385d5e5ce4b114a7da8bc9
Andrewf9001 <andrewf9001@gmail.com>
2024-10-29 16:43:02 -0600

    added navlink and routed for photo transformer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 
+import PhotoTransformer from "./pages/PhotoTransformer";
 import BoxShadowPage from "./pages/BoxShadowPage";
-import Homepage from "./pages/Homepage";
 import Navbar from "./navigation/Navbar";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/box-shadow" element={<BoxShadowPage />} />
+        <Route path="/photo-transformer" element={<PhotoTransformer />} />
       </Routes>
     </div>
   );
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <Link to="/">HomeLogo</Link>
+
       <NavLink to="/box-shadow">Box Shadow</NavLink>
+      <NavLink to="/photo-transformer">Photo Transformer</NavLink>
     </div>
   );
 };
