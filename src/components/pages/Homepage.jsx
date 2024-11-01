9e142f1da8e41110ea30b5bb29c2a6b82a1b471c
Andrewf9001 <andrewf9001@gmail.com>
2024-11-01 13:35:12 -0600

    added homepage styles, updated content

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 const Homepage = () => {
   return (
     <div className="homepage-container">
-      <h1>Widgets</h1>
+      <h1>React Widgets!</h1>
+
+      <div>render widget links</div>
     </div>
   );
 };
