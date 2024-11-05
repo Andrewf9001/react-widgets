164fad502d9d55c4b95fb3063d2f52cfca011d5c
Andrewf9001 <andrewf9001@gmail.com>
2024-11-05 16:04:03 -0700

    added rock paper scissors page

---
+++ src/components/pages/RockPaperScissors.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { rockPaperScissors } from "../helpers/helperArrays";
+
+const RockPaperScissors = () => {
+  const [playerChoice, setPlayerChoice] = useState("");
+  const [compChoice, setCompChoice] = useState("");
+  const [winner, setWinner] = useState("");
+  const [showComp, setShowComp] = useState(false);
+
+  const gameResults = () => {
+    setShowComp(true);
+
+    if (playerChoice === "Rock" && compChoice === "Scissors") {
+      setWinner("Player Wins!");
+    } else if (playerChoice === "Rock" && compChoice === "Paper") {
+      setWinner("Computer Wins!");
+    } else if (playerChoice === "Scissors" && compChoice === "Paper") {
+      setWinner("Player Wins!");
+    } else if (playerChoice === "Scissors" && compChoice === "Rock") {
+      setWinner("Computer Wins!");
+    } else if (playerChoice === "Paper" && compChoice === "Rock") {
+      setWinner("Player Wins!");
+    } else if (playerChoice === "Paper" && compChoice === "Scissors") {
+      setWinner("Computer Wins!");
+    } else {
+      setWinner("Tie Game!");
+    }
+  };
+
+  const compPick = () => {
+    setCompChoice(
+      rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)]
+    );
+  };
+
+  const handleResetGame = () => {
+    setPlayerChoice("");
+    setCompChoice("");
+    setWinner("");
+    // setShowComp(false);
+  };
+
+  useEffect(async () => {
+    compPick();
+    setShowComp(false);
+  }, [playerChoice]);
+
+  return (
+    <div className="rock-paper-scissors-container">
+      <div className="game-choice-container">
+        <div className="choice-wrapper player">
+          <h1 className="label">Player: </h1>
+          <h1>{playerChoice}</h1>
+        </div>
+
+        <div className="choice-wrapper comp">
+          <h1 className="label">Computer: </h1>
+          <h1>{showComp && compChoice}</h1>
+        </div>
+
+        <div className="winner-wrapper">{winner}</div>
+      </div>
+
+      <div className="game-btn-wrapper">
+        <FontAwesomeIcon
+          icon="fa-solid fa-hand-back-fist"
+          onClick={() => setPlayerChoice("Rock")}
+        />
+
+        <FontAwesomeIcon
+          icon="fa-solid fa-hand"
+          onClick={() => setPlayerChoice("Paper")}
+        />
+
+        <FontAwesomeIcon
+          icon="fa-solid fa-hand-scissors"
+          onClick={() => setPlayerChoice("Scissors")}
+        />
+      </div>
+
+      <div className="submit-reset-wrapper">
+        <button onClick={gameResults}>Submit Choice</button>
+
+        <button onClick={handleResetGame} className="reset-game">
+          Play Again?
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RockPaperScissors;
