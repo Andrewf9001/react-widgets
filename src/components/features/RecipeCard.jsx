5c2b331ef3d904b7cf4fedec2d2502a01c8d80bd
Andrewf9001 <andrewf9001@gmail.com>
2024-11-18 17:06:22 -0700

    fixed errors

--- src/components/features/RecipeCard.jsx
+++ src/components/features/RecipeCard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "../styles/main.scss";
-
 const RecipeCard = (props) => {
   const recipe = props.recipe;
 
--- src/components/pages/Recipe.jsx
+++ src/components/pages/Recipe.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import RecipeCard from "./recipeCard";
-import "../styles/main.scss";
+import RecipeCard from "../features/RecipeCard";
 
 const Recipe = () => {
   const [query, setQuery] = useState("");
